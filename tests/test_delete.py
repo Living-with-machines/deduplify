e1a6644a20df678e8c87415b9febb250fd741b01
Sarah Gibson <sgibson@turing.ac.uk>
2020-09-08 21:38:54 +0100

    Restructure empty dir test

--- tests/test_delete.py
+++ tests/test_delete.py
@@ -6,8 +6,10 @@ from deduplify.del_empty_dirs import empty_dir_search
 @patch("deduplify.del_empty_dirs.os.rmdir")
 def test_del_empty_dirs(mock):
     test_dir = os.path.join("tests", "testdir")
-    os.mkdir(test_dir)
     test_call = [call(os.path.abspath(test_dir))]
+
+    if not os.path.exists(test_dir):
+        os.mkdir(test_dir)
 
     here = os.path.dirname(__file__)
     empty_dir_search(here)
