bfbbb9459686dd2aa206c8e07407608b8c7875c1
Sarah Gibson <sgibson@turing.ac.uk>
2020-09-09 13:11:00 +0100

    Add test for hash_files.filter_dict

---
+++ tests/test_hash.py
@@ -0,0 +1,13 @@
+from deduplify.hash_files import filter_dict
+
+
+def test_filter_dict():
+    test_dict = {
+        "hash1": ["filepath1"],
+        "hash2": ["filepath2", "filepath3"]
+    }
+
+    dupdict, undict = filter_dict(test_dict)
+
+    assert dupdict == {"hash2": ["filepath2", "filepath3"]}
+    assert undict == {"hash1": "filepath1"}
