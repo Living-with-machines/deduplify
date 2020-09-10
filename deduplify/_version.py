bd5a3b29a509c10db3a6d94f850e14aa3cbfd51a
Sarah Gibson <sgibson@turing.ac.uk>
2020-09-10 20:24:20 +0100

    Run formatting update

--- deduplify/_version.py
+++ deduplify/_version.py
@@ -7,5 +7,5 @@ Provides deduplify version information.
 
 from incremental import Version
 
-__version__ = Version('deduplify', 20, 9, 0)
+__version__ = Version("deduplify", 20, 9, 0)
 __all__ = ["__version__"]
