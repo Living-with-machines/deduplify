a10677b39e15fb64413bbfff53558a6396e0ed2a
Sarah Gibson <sgibson@turing.ac.uk>
2021-03-22 20:28:31 +0000

    Version bump

--- deduplify/_version.py
+++ deduplify/_version.py
@@ -7,5 +7,5 @@ Provides deduplify version information.
 
 from incremental import Version
 
-__version__ = Version("deduplify", 0, 1, 2)
+__version__ = Version("deduplify", 0, 2, 0)
 __all__ = ["__version__"]
