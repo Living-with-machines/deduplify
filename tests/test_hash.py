59c38354b2f48f17e36477c722004430db78a16e
Sarah Gibson <sgibson@turing.ac.uk>
2021-03-24 17:09:31 +0000

    Add tests for transform_dict and restart_run funcs

--- tests/test_hash.py
+++ tests/test_hash.py
@@ -1,5 +1,12 @@
 import os
-from deduplify.hash_files import filter_dict, get_total_number_of_files, hashfile
+from collections import defaultdict
+from deduplify.hash_files import (
+    filter_dict,
+    get_total_number_of_files,
+    hashfile,
+    transform_dict,
+    restart_run,
+)
 
 
 def test_filter_dict():
@@ -28,3 +35,33 @@ def test_hashfile():
 
     assert md5_hash == "f3fb257d843b252bdc0442402552d840"
     assert outpath == path
+
+
+def test_transform_dict():
+    test_dict = {"key1": "value1", "key2": "value2"}
+    expected = {"key1": ["value1"], "key2": ["value2"]}
+
+    output = transform_dict(test_dict)
+
+    assert output == expected
+
+
+def test_restart_run():
+    dup_file = os.path.join(os.getcwd(), "tests", "assets", "test_duplicates.json")
+    un_file = os.path.join(os.getcwd(), "tests", "assets", "test_uniques.json")
+
+    expected_dict = defaultdict(
+        list,
+        {
+            "key1": ["valueA", "valueB"],
+            "key2": ["valueC", "valueD"],
+            "key3": ["valueE"],
+            "key4": ["valueF"],
+        },
+    )
+    expected_list = ["valueA", "valueB", "valueC", "valueD", "valueE", "valueF"]
+
+    hashes, files_to_be_skipped = restart_run(dup_file, un_file)
+
+    assert hashes == expected_dict
+    assert files_to_be_skipped == expected_list
