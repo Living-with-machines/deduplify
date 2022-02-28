712571f58071a07f17735db7b8cbb9f89571bbfe
Sarah Gibson <drsarahlgibson@gmail.com>
2022-02-28 17:59:19 +0000

    Add --ext cli arg

--- deduplify/cli.py
+++ deduplify/cli.py
@@ -86,6 +86,11 @@ def parse_args(args):
         default="file_hashes.json",
         help="Destination database for file hashes. Must be a JSON file. Default: file_hashes.json",
     )
+    parser.add_argument(
+        "--exts",
+        nargs="?",
+        help="A list of file extensions to search for."
+    )
     parser_hash.add_argument(
         "--restart",
         action="store_true",
