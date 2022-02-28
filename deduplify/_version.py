bf9fd6ac8db694b5aee3aebe03420a849564cc70
Sarah Gibson <drsarahlgibson@gmail.com>
2022-02-28 18:11:23 +0000

    Bump minor version

--- deduplify/_version.py
+++ deduplify/_version.py
@@ -7,5 +7,5 @@ Provides deduplify version information.
 
 from incremental import Version
 
-__version__ = Version("deduplify", 0, 3, 0)
+__version__ = Version("deduplify", 0, 4, 0)
 __all__ = ["__version__"]
